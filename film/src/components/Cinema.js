cc4ee469c02a58026ef5d8ae0c5d6cbe06549f0b
minhu76 <minhu7676@gmail.com>
2019-10-27 18:04:16 +0700

    update logo

--- film/src/components/Cinema.js
+++ film/src/components/Cinema.js
@@ -8,10 +8,6 @@ export default class Cinema extends Component {
             <div className="card-columns">
                 <div className="card">
                     <img src={cinema.logo} width={"70px"} height={"70px"} />
-                    {/* <div className="card-body">
-                        <h4 className="card-title">{cinema.maHeThongRap}</h4>
-                        <p className="card-text">{cinema.tenHeThongRap}</p>
-                    </div> */}
                 </div>
             </div>
         )
