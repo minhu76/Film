8f0ea1a9cacbccf34f9d80d0408e8e99e36dd05e
minhu76 <minhu7676@gmail.com>
2019-11-15 11:44:52 +0700

    update component Book

--- film/src/Pages/HomePage.js
+++ film/src/Pages/HomePage.js
@@ -3,15 +3,16 @@ import Carousel from './../components/Carousel';
 import ListMovies from './ListMovies/ListMovies';
 import ListCinemas from './Cinemas/ListCinemas';
 import ListNews from './News/ListNews';
-import ListBook from './../components/Book.js/ListBook';
+//import ListBook from './../components/Book.js/ListBook';
 import _body from './../SCSS/Components/Body/_body.scss';
+import BookMovie from '../components/Book.js/BookMovie';
 
 export default class HomePage extends Component {
     render() {
         return (
             <div className="myBody">
                 <Carousel />
-                <ListBook />
+                <BookMovie />
                 <ListMovies />
                 <ListCinemas />
                 <ListNews />
